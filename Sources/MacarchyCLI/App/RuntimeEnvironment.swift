e719a344790e999d562f5850007dfea83f10c549
Ramtin Javanmardi <ramme95@gmail.com>
2026-09-10 18:42:53 +0200

    chore(release): prepare v0.9.4

--- Sources/MacarchyCLI/App/RuntimeEnvironment.swift
+++ Sources/MacarchyCLI/App/RuntimeEnvironment.swift
@@ -23,7 +23,7 @@ struct InvalidBuildInformationError: Error, CustomStringConvertible, Equatable {
 }
 
 struct RuntimeEnvironment: Sendable {
-  static let sourceVersion = "0.9.3"
+  static let sourceVersion = "0.9.4"
 
   static let live = RuntimeEnvironment(
     executableURL: Bundle.main.executableURL
